d4ee8c93a4bf65a75353ad46721f27d4989887d8
unsel <tunsel1@binghamton.edu>
2020-12-26 16:46:52 +0300

    tent

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,30 @@
 import React,{useState,useEffect} from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
+// import { firestore } from '../../firebase/firebase.utils';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import { firestore } from '../../firebase/firebase.utils';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = () => {
+const ShopPage = ({match}) => { // route automatically pass 3 objects -> match,location,history
 
     // eslint-disable-next-line
-    const [collections,setCollections] = useState(SHOP_DATA)
+    // const [collections,setCollections] = useState(SHOP_DATA)
 
-    var unsubscribeFromSnapshot=null;
-    useEffect(()=>{
+    // var unsubscribeFromSnapshot=null;
+    // useEffect(()=>{
 
-        const collectionRef=firestore.collection('users');
-        collectionRef.onSnapshot(async snapshot =>{
-            console.log(snapshot.data);
-        })
-    })
+    //     const collectionRef=firestore.collection('users');
+    //     collectionRef.onSnapshot(async snapshot =>{
+    //         console.log(snapshot.data);
+    //     })
+    // })
 
     return (
         <div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
+            <Route exact path={`${match.path}`} component={CollectionOverview}/>
+            <Route  path={`${match.path}/:collectionId`} component={CollectionPage}/>
         </div>
     )
 } 
-
-
 
 export default ShopPage;
